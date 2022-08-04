6b0c8b55c41cc397541e46ce860190f813a41523
Pierre <R2Jeu.prive@gmail.com>
2022-08-04 21:09:23 +0200

    styled end screen

--- client/js/end.js
+++ client/js/end.js
@@ -4,12 +4,12 @@ $(document).ready(function() {
         
         $("#end-screen .table").empty();
         $("#end-screen .table").append("<tr class='header'></tr>")
-        $("#end-screen .header").append("<th></th>")
+        $("#end-screen .header").append("<th class='cells'>Joueurs</th>")
         for(let [i, playerScore] of data.scores[0].entries()){
             if(i == 0){continue;}
-            $("#end-screen .header").append("<th>Q" + i + "</th>")
+            $("#end-screen .header").append("<th class='cells'>Q" + i + "</th>")
         }
-        $("#end-screen .header").append("<th>Total<th>")
+        $("#end-screen .header").append("<th class='cells'>Total</th>")
 
         while(true){
 			let sorted = true;
@@ -31,6 +31,8 @@ $(document).ready(function() {
 			}
 		}
 
+        $("#end-screen .winner").text(data.scores[0][0] + " est Champion de WikiMasters !")
+
         for(let [i, playerScores] of data.scores.entries()){
             $("#end-screen .table").append("<tr id='player" + i + "'></tr>")
             let totalScore = 0;
@@ -39,9 +41,13 @@ $(document).ready(function() {
                     totalScore += playerScore;
                     playerScore = (playerScore/1000) + "s";
                 }
-                $("#player" + i).append("<td>" + playerScore + "</td>")
+                $("#player" + i).append("<td class='cells'>" + playerScore + "</td>")
             }
-            $("#player" + i).append("<td>" + (totalScore/1000) + "s</td>")
+            let min = Math.floor(totalScore/60000);
+			let sec = ("0" + (Math.floor((totalScore%60000)/1000))).slice(-2);
+			let mil = ("00" + (totalScore%1000)).slice(-3);
+			let time = min + ":" + sec + "." + mil + "s";
+            $("#player" + i).append("<td class='cells'>" + time + "</td>")
         }
 
         $("#correction-screen").addClass("hidden-screen");
