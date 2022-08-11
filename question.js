251c4585cbc232186d353106848cd1a1fb2125a8
Pierre <R2Jeu.prive@gmail.com>
2022-08-11 12:37:18 +0200

    fixed bug

--- question.js
+++ question.js
@@ -136,7 +136,7 @@ function GetDescriptionFromContent(r, title){
 	for (let i = 0; i < startIndexes.length; i++) {
 		paragraphs.push(r.substring(startIndexes[i],endIndexes[i]));
 
-        if(paragraphs.at(-1).indexOf(title) != -1 && paragraphs.at(-1).indexOf(title.toLowerCase())){
+        if(paragraphs.at(-1).indexOf(title) != -1 && paragraphs.at(-1).indexOf(title.toLowerCase()) != -1){
             if(paragraphs.at(-1).indexOf(title.toLowerCase()) == -1 && paragraphs.at(-1).indexOf(" un peuple ") == -1){
 				console.log("PROPRE " + title);
                 return false;
