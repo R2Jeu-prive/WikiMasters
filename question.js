c3a1e1bdb21a6016178db51cf768e65aa12a6489
Pierre <73789471+R2Jeu-prive@users.noreply.github.com>
2022-08-22 19:50:48 +0000

    testing json parsing

--- question.js
+++ question.js
@@ -1,10 +1,17 @@
 const fetch = require('node-fetch');
+const fs = require("fs");
 let pages;
 let questions;
 
 function Init(){
 	pages = [];
 	questions = [];
+	let rawdata = fs.readFileSync('data/7.json');
+	let text = rawdata.toString()
+	console.log(text)
+	console.log(JSON.parse(text.slice(1)))
+	/*let file = JSON.parse(rawdata);
+	console.log(file);*/
 }
 
 function Fetch(){
